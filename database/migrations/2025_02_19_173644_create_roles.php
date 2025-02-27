9b0c75b7f85e34885e92435531f93f7fde30b310
Boris <boriscruzr@gmail.com>
2025-02-27 17:48:13 -0300

    Se corrigio el inicio de sesion para admin

--- database/migrations/2025_02_19_173644_create_roles.php
+++ database/migrations/2025_02_19_173644_create_roles.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Schema;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 return new class extends Migration
 {
     /**
@@ -15,15 +17,30 @@ return new class extends Migration
     {
         // Crear el rol "Admin"
         $role = Role::create(['name' => 'Admin']);
-    
-        // Buscar al usuario por su correo electrónico
-        $user = User::where('email', 'boris@admin.com')->first();
-    
-        // Si el usuario existe, asignarle el rol "Admin"
-        if ($user) {
+
+        // Verificar si la tabla `users` está vacía
+        if (User::count() === 0) {
+            // Crear un usuario administrador por defecto
+            $user = User::create([
+                'name' => 'Admin',
+                'dni'=> 00000000,
+                'email' => 'boris@admin.com',
+                'password' => Hash::make('borisadminB@1'), // Cambia 'password' por una contraseña segura
+            ]);
+
+            // Asignar el rol "Admin" al usuario creado
             $user->assignRole($role);
+        } else {
+            // Si ya existe algún usuario, buscar al usuario con el correo "boris@admin.com"
+            $user = User::where('email', 'boris@admin.com')->first();
+
+            // Si el usuario existe, asignarle el rol "Admin"
+            if ($user) {
+                $user->assignRole($role);
+            }
         }
     }
+
 
     /**
      * Reverse the migrations.
