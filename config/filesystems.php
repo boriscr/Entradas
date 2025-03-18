93d53f6bede207fd09079f0ff14b4c30e4ae33b7
Boris <boriscruzr@gmail.com>
2025-03-18 17:24:12 -0300

    modificacion de region

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'), // Usa 'auto' o 'us-east-1'
+            'region' => env('AWS_DEFAULT_REGION', 'auto'), // Usa 'auto' o 'us-east-1'
             'bucket' => env('AWS_BUCKET'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
